664c8e87b4a647da24160b3b4514807f9517655e
cyberc3dr <47525358+cyberc3dr@users.noreply.github.com>
2026-06-02 14:59:44 +0000

    Revert all uname spoof features.

--- kernel/sys.c
+++ kernel/sys.c
@@ -44,7 +44,6 @@
 #include <linux/ctype.h>
 #include <linux/mm.h>
 #include <linux/mempolicy.h>
-#include <linux/string_helpers.h>
 
 #include <linux/compat.h>
 #include <linux/syscalls.h>
@@ -1210,24 +1209,6 @@ DECLARE_RWSEM(uts_sem);
 #define override_architecture(name)	0
 #endif
 
-static void override_custom_release(char __user *release, size_t len)
-{
-#ifdef CONFIG_UNAME_OVERRIDE
-	char *buf;
-
-	buf = kstrdup_quotable_cmdline(current, GFP_KERNEL);
-	if (buf == NULL)
-		return;
-
-	if (strstr(buf, CONFIG_UNAME_OVERRIDE_TARGET)) {
-		copy_to_user(release, CONFIG_UNAME_OVERRIDE_STRING,
-			       strlen(CONFIG_UNAME_OVERRIDE_STRING) + 1);
-	}
-
-	kfree(buf);
-#endif
-}
-
 /*
  * Work around broken programs that cannot handle "Linux 3.0".
  * Instead we map 3.x to 2.6.40+x, so e.g. 3.0 would be 2.6.40
@@ -1259,28 +1240,6 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-static int override_version(struct new_utsname __user *name)
-{
-#ifdef CONFIG_F2FS_REPORT_FAKE_KERNEL_VERSION
-	int ret;
-
-	if (strcmp(current->comm, "fsck.f2fs"))
-		return 0;
-
-	ret = copy_to_user(name->release, CONFIG_F2FS_FAKE_KERNEL_RELEASE,
-			   strlen(CONFIG_F2FS_FAKE_KERNEL_RELEASE) + 1);
-	if (ret)
-		return ret;
-
-	ret = copy_to_user(name->version, CONFIG_F2FS_FAKE_KERNEL_VERSION,
-			   strlen(CONFIG_F2FS_FAKE_KERNEL_VERSION) + 1);
-
-	return ret;
-#else
-	return 0;
-#endif
-}
-
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1294,8 +1253,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (override_release(name->release, sizeof(name->release)))
 		return -EFAULT;
 	if (override_architecture(name))
-		return -EFAULT;
-	if (override_version(name))
 		return -EFAULT;
 	return 0;
 }
@@ -1317,7 +1274,6 @@ SYSCALL_DEFINE1(uname, struct old_utsname __user *, name)
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
-	override_custom_release(name->release, sizeof(name->release));
 	if (override_release(name->release, sizeof(name->release)))
 		return -EFAULT;
 	if (override_architecture(name))
